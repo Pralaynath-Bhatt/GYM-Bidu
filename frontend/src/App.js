ebc5fad01e782cd0385a78bd8e4756590d3802b7
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-03-12 00:28:18 +0530

    adding ui from librarys available in the market

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-
+import ResponsiveAppBar from "./ResponsiveAppBar.js"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ResponsiveAppBar/>
         Lets start the actual coding
       </header>
     </div>
