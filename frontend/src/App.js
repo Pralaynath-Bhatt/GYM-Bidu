cb226f458302051270a2dbfbd047ab281a3ad0cf
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-03-11 23:54:57 +0530

    Lets start the front end now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        Lets start the actual coding
       </header>
     </div>
   );
